4318b3a76c168fa240fd945493ae4a41652673cd
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-10-29 13:40:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,28 +7,27 @@ var n = 10;
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
 
-async function load () { 
-  for (var i = 0; i <=n; i++) {
+async function load() {
+	for (var i = 0; i <= n; i++) {
 
-  var username = user + n++;
-const email1 = new TM(username);
-   const requestBody = 
-{
-"data": {
-"attributes":{
- "firstName":"Wahyuni",
- "lastName":"Ida",
- "displayName": username,
- "email": email1,
- "password":pass}
-}
-}
-var url = "https://kotakode.com/api/v1/users";
-axios.post(url ,requestBody)
-  .then((result) => {
-var i = result.data;
+		var username = user + n++;
+		const requestBody = {
+			"data": {
+				"attributes": {
+					"firstName": "Wahyuni",
+					"lastName": "Ida",
+					"displayName": username,
+					"email": email1,
+					"password": pass
+				}
+			}
+		}
+		var url = "https://kotakode.com/api/v1/users";
+		axios.post(url, requestBody)
+			.then((result) => {
+				var i = result.data;
 
-console.log(`
+				console.log(`
  Success Creating Kotakode Account 
 ID : ${i.data[0].id}
 Type : ${i.data[0].type}
@@ -36,28 +35,24 @@ Email : ${username}@gmail.com
 Password : ${pass}
 `)
 
-fs.writeFile('akun.txt', username +"@gmail.com:"+ password +"\n",  {'flag':'a'},  function(err) {
-         if (err) {
-       console.error("gagal"+ err);
+				fs.writeFile('akun.txt', username + "@gmail.com:" + password + "\n", {
+					'flag': 'a'
+				}, function (err) {
+					if (err) {
+						console.error("gagal" + err);
 
-}
-    })
-
-
-
-
-}
-)        
+					}
+				})
 
 
+			})
 
-  .catch((err) => {
-console.log("Error when creating account");
-  })
-await timer(3000);
-}
+
+			.catch((err) => {
+				console.log("Error when creating account");
+			})
+		await timer(3000);
+	}
 }
 
 load();
-
-
